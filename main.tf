8392722d6d0cf05f6eded977f1bdd663a33e9dd4
telkynsolutions <53535763+telkynsolutions@users.noreply.github.com>
2024-10-09 20:28:56 +0100

    updates to code

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b4c7755cdf0d9219"
+  instance_type = "t2.micro"
+  key_name  = "greenhydra"
+  tags = {
+    Name = "Node1"
+  }
+}
