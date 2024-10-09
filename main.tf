6cf9521eae123c983614b2fa54743bb48020062d
telkynsolutions <53535763+telkynsolutions@users.noreply.github.com>
2024-10-09 23:26:07 +0100

    Security group added to code

--- main.tf
+++ main.tf
@@ -13,11 +13,31 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+  resource "aws_security_group" "allow_ssh" {
+    name = "allow ssh"
+  # ... other configuration ...
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+   }
+  }
 resource "aws_instance" "app_server" {
   ami           = "ami-0b4c7755cdf0d9219"
   instance_type = "t2.micro"
   key_name = "greenhydra"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     Name = "Node1"
-  }
+   }
 }
